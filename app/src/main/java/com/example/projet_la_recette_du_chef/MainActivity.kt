a705c834693155c30125bfbbfcfd592586a32906
TheGooodDev <72625343+TheGooodDev@users.noreply.github.com>
2022-10-28 12:25:04 +0200

    Project Base

---
+++ app/src/main/java/com/example/projet_la_recette_du_chef/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projet_la_recette_du_chef
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
